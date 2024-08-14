9e09f5b966c2b6775fc60394fc14b20855e38870
AlexProher <alexproher@gmail.com>
2024-08-14 15:52:00 +0200

    see previous commit

---
+++ SDM_model_Matlab/results_treat.m
@@ -0,0 +1,63 @@
+%%
+% load('nominal.mat', 'results');
+load('worstCase.mat', 'results');
+
+%%
+figure;
+subplot(3,1,1);
+
+hold on;
+plot(results.pid_time, results.pid(:,1)-1.2,  "LineStyle","--", LineWidth=2);
+plot(results.hinf_time, results.hinf(:,1)-1.2,  "LineStyle","-.", LineWidth=2);
+plot(results.mu_time, results.mu(:,1)-1.2,  "LineStyle",":", LineWidth=2);
+plot(results.ol_time, results.ol(:,1)-1.2, "LineStyle","-", LineWidth=2);
+
+grid on;
+legend("PID", "Hinf", "mu-synth", "openLoop")
+fontsize(14, 'points');
+title("Nomianl Model Wheel Vertical Displacement")
+% title("WorstCase Model Wheel Vertical Displacement")
+xlabel("Time, s");
+ylabel("Displacement, m");
+xlim([3,10]);
+
+subplot(3,1,2);
+
+plot(results.pid_time, results.pid(:,2),  "LineStyle","--", LineWidth=2);
+hold on;
+plot(results.hinf_time, results.hinf(:,2),  "LineStyle","-.", LineWidth=2);
+plot(results.mu_time, results.mu(:,2),  "LineStyle",":", LineWidth=2);
+plot(results.ol_time, results.ol(:,2)-0.2, "LineStyle","-", LineWidth=2);
+grid on;
+% legend("PID", "Hinf", "mu-synth", "OL")
+fontsize(14, 'points');
+% title("WorstCase Model Body Vertical Displacement")
+title("Nomianl Model Body Vertical Displacement")
+xlabel("Time, s");
+ylabel("Displacement, m");
+xlim([3,10]);
+
+subplot(3,1,3);
+
+plot(results.pid_time, results.pid(:,3),  "LineStyle","--", LineWidth=2);
+hold on;
+plot(results.hinf_time, results.hinf(:,3),  "LineStyle","-.", LineWidth=2);
+plot(results.mu_time, results.mu(:,3),  "LineStyle",":", LineWidth=2);
+grid on;
+% legend("PID", "Hinf", "mu-synth")
+fontsize(14, 'points');
+% title("WorstCase Model Controller Out")
+title("Nomianl Wheel Controller Out")
+xlabel("Time, s");
+ylabel("Force, N");
+xlim([3,10]);
+
+%%
+metrics = struct();
+ce.pid = trapz(results.pid_time, results.pid(:, 3).^2);
+ce.hinf = trapz(results.hinf_time, results.hinf(:, 3).^2);
+ce.mu = trapz(results.mu_time, results.mu(:, 3).^2);
+
+iae.pid = sum(abs(results.pid(:,2)));
+iae.hinf = sum(abs(results.hinf(:,2)));
+iae.mu = sum(abs(results.mu(:,2)));
